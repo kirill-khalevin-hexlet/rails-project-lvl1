91e17ef66a91bded32f28ee23a96664e4984aa41
Kirill Khalevin <kkh@level.travel>
2021-10-30 14:41:47 +0300

    add HexletCode::Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,15 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Tag
+    SELF_CLOSING_TAGS = %w(area base basefont br col frame hr img input isindex link meta param).freeze
+
+    def self.build(tag, **kwargs, &block)
+      tag_attrs = kwargs.map { |attr, param| " #{attr}=\"#{param}\"" }.join
+      inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag) ? "" : "#{yield if block_given?}<#{tag}>"
+      html_block = "<#{tag}#{tag_attrs}>#{inner_and_closed_part}"
+    end
+  end
+
 end
