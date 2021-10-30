946e348eefab06b4e8235c5e71d505f72bbc30fa
Kirill Khalevin <kkh@level.travel>
2021-10-30 14:56:17 +0300

    Add Rubocop and warning fixes

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,14 +5,15 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
+  # Tag builder
   module Tag
-    SELF_CLOSING_TAGS = %w(area base basefont br col frame hr img input isindex link meta param).freeze
+    SELF_CLOSING_TAGS = %w[area base basefont br col frame hr img input isindex link meta param].freeze
 
-    def self.build(tag, **kwargs, &block)
+    def self.build(tag, **kwargs, &_block)
       tag_attrs = kwargs.map { |attr, param| " #{attr}=\"#{param}\"" }.join
       inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag) ? "" : "#{yield if block_given?}<#{tag}>"
-      html_block = "<#{tag}#{tag_attrs}>#{inner_and_closed_part}"
+
+      "<#{tag}#{tag_attrs}>#{inner_and_closed_part}"
     end
   end
-
 end
