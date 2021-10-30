b4335950191255b3e9e84d99faac362b15ca7e98
Kirill Khalevin <kkh@level.travel>
2021-10-30 22:04:56 +0300

    Create empty form

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,13 +5,17 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
+  def self.form_for(user, **kwargs, &_block)
+    Tag.build("form", action: kwargs[:action] || "#", method: :post)
+  end
+
   # Tag builder
   module Tag
     SELF_CLOSING_TAGS = %w[area base basefont br col frame hr img input isindex link meta param].freeze
 
     def self.build(tag, **kwargs, &_block)
       tag_attrs = kwargs.map { |attr, param| " #{attr}=\"#{param}\"" }.join
-      inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag) ? "" : "#{yield if block_given?}<#{tag}>"
+      inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag) ? "" : "#{yield if block_given?}</#{tag}>"
 
       "<#{tag}#{tag_attrs}>#{inner_and_closed_part}"
     end
