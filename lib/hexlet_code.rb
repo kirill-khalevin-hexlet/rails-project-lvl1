a074a6bc613495d1c15547c441341e3ad1552952
Kirill Khalevin <kirill.khalevin.hexlet@gmail.com>
2021-10-31 18:41:49 +0300

    Add method form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,11 +2,14 @@
 
 require_relative "hexlet_code/version"
 
+# Lesson, create gem with Hexlet
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(user, **kwargs, &_block)
-    Tag.build("form", action: kwargs[:action] || "#", method: :post)
+  def self.form_for(obj, **kwargs, &_block)
+    form_obj = FormObj.new(obj)
+    yield form_obj
+    Tag.build("form", action: kwargs[:action] || "#", method: :post) { "\n#{form_obj.build_body}\n" }
   end
 
   # Tag builder
@@ -14,10 +17,40 @@ module HexletCode
     SELF_CLOSING_TAGS = %w[area base basefont br col frame hr img input isindex link meta param].freeze
 
     def self.build(tag, **kwargs, &_block)
+      tag_build = tag.to_s.downcase
       tag_attrs = kwargs.map { |attr, param| " #{attr}=\"#{param}\"" }.join
-      inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag) ? "" : "#{yield if block_given?}</#{tag}>"
+      inner_and_closed_part = SELF_CLOSING_TAGS.include?(tag_build) ? "" : "#{yield if block_given?}</#{tag_build}>"
 
-      "<#{tag}#{tag_attrs}>#{inner_and_closed_part}"
+      "<#{tag_build}#{tag_attrs}>#{inner_and_closed_part}"
+    end
+  end
+
+  # struct_object with tags generator by block
+  class FormObj
+    def initialize(struct_object)
+      @struct_object = struct_object
+      @body_tags = []
+    end
+
+    def input(name_column, **kwargs)
+      add_tag = :input
+      value = @struct_object.method(name_column.to_sym).call
+      if kwargs[:as] == :text
+        add_tag = :textarea
+        attrs = { cols: 20, rows: 40, name: name_column }
+        @body_tags << Tag.build(add_tag, attrs) { value }
+      else
+        attrs = { name: name_column, type: :text, value: value }
+        @body_tags << Tag.build(add_tag, attrs)
+      end
+    end
+
+    def submit
+      @body_tags << Tag.build(:input, { type: :submit })
+    end
+
+    def build_body
+      @body_tags.join("\n")
     end
   end
 end
